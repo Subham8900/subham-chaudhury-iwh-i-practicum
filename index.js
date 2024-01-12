fb299760deac5cb5c992c9535a6f8bafddf9de1c
Subham8900 <124257190+Subham8900@users.noreply.github.com>
2024-01-12 13:39:52 +0530

    final commit in the practium

--- index.js
+++ index.js
@@ -1,26 +1,64 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "pug");
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = "pat-na1-b5ad8d94-c161-4073-92d0-36facca15119";
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get("/", async (req, res) => {
+  const contacts = "https://api.hubapi.com/crm/v3/objects/contacts";
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
+
+  // In a real application, you would make a GET request to retrieve CRM record data
+  // For now, using dummy data
+  let crmRecords = {};
+
+  try {
+    const pageTitle = "CRM Records Homepage";
+
+    const resp = await axios.get(contacts, { headers });
+    console.log("here data", resp.data.results);
+    const crmRecords = resp.data.results;
+    res.render("homepage", { pageTitle, crmRecords });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get("/updated-cobj", (req, res) => {
+  // Your existing form submission logic here
+
+  // For demonstration purposes, let's redirect to a success page
+  res.render("updates");
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+app.post("/updated-cobj", (req, res) => {
+  // Process form data and create CRM record here
+  // Replace the following lines with your CRM record creation logic
+
+  const { name, email } = req.body;
+  // Your CRM record creation logic goes here
+
+  // Redirect back to the homepage after creating the CRM record
+  res.redirect("/");
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
@@ -66,6 +104,5 @@ app.post('/update', async (req, res) => {
 });
 */
 
-
 // * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
