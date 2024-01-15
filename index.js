0ed728132c63081b41bf159999d77fe62d5066be
shubham chaudhury <subham.chaudhury@tuteck.com>
2024-01-15 16:13:19 +0530

    final commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PRIVATE_APP_ACCESS = "pat-na1-b5ad8d94-c161-4073-92d0-36facca15119";
 
 // * Code for Route 1 goes here
 app.get("/", async (req, res) => {
-  const contacts = "https://api.hubapi.com/crm/v3/objects/contacts";
+  const contacts = "https://api.hubapi.com/crm/v3/objects/2-22478587";
   const headers = {
     Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
     "Content-Type": "application/json",
@@ -23,13 +23,31 @@ app.get("/", async (req, res) => {
   // In a real application, you would make a GET request to retrieve CRM record data
   // For now, using dummy data
   let crmRecords = {};
+  let body = {
+    properties: ["income", "name", "superpower"],
+    inputs: [],
+  };
 
   try {
     const pageTitle = "CRM Records Homepage";
 
     const resp = await axios.get(contacts, { headers });
-    console.log("here data", resp.data.results);
-    const crmRecords = resp.data.results;
+    let id_list = [];
+    resp.data.results.map((val, index) => {
+      id_list.push(val.id);
+    });
+    console.log("here data", id_list);
+    id_list.map((val, index) => {
+      body.inputs.push({ id: val });
+    });
+    console.log("body data", body);
+    const detailResp = await axios.post(
+      "https://api.hubapi.com/crm/v3/objects/2-22478587/batch/read",
+      body,
+      { headers }
+    );
+    console.log("final data", detailResp.data.results);
+    const crmRecords = detailResp.data.results;
     res.render("homepage", { pageTitle, crmRecords });
   } catch (error) {
     console.error(error);
@@ -49,15 +67,35 @@ app.get("/updated-cobj", (req, res) => {
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
-app.post("/updated-cobj", (req, res) => {
+app.post("/updated-cobj", async (req, res) => {
   // Process form data and create CRM record here
   // Replace the following lines with your CRM record creation logic
+  console.log("req", req.body);
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
 
-  const { name, email } = req.body;
+  const { name, income, superPower } = req.body;
+  const properties = {
+    properties: {
+      income: income,
+      name: name,
+      superpower: superPower,
+    },
+  };
   // Your CRM record creation logic goes here
+  const newDataResp = await axios.post(
+    "https://api.hubapi.com/crm/v3/objects/2-22478587",
+    properties,
+    { headers }
+  );
+  if (newDataResp) {
+    console.log("new resp".newDataResp);
+    res.redirect("/");
+  }
 
   // Redirect back to the homepage after creating the CRM record
-  res.redirect("/");
 });
 
 /** 
